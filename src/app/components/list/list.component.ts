2b1572ce2195b905aa128d99f7af163e316178e1
Hamza <hamzaazman6776@gmail.com>
2021-03-14 23:35:06 +0300

    Operations created

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { getMatIconFailedToSanitizeLiteralError } from '@angular/material/icon';
+import { Todo } from 'src/app/models/todo';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.todos = this.todoService.getTodos();
   }
 
+  todos: Todo[];
+
+  addTodo(todoText: HTMLInputElement) {
+    const obj = {
+      text: todoText.value,
+      createdAt: new Date(),
+    };
+    this.todoService.addTodo(obj);
+    todoText.value = '';
+  }
+  removeTodo(todo: Todo) {
+    this.todoService.removeTodo(todo);
+  }
 }
